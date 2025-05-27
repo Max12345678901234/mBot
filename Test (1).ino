cb67df30a7e71939c7defa52acff2f6c0b603f61
Max12345678901234 <146214769+Max12345678901234@users.noreply.github.com>
2025-05-27 10:48:23 +0200

    Geradeauslauf.ino

--- Test (1).ino
+++ Test (1).ino
@@ -1,72 +1,70 @@
+#include "MeMCore.h"                       //Bibliothek für M-Bot initialisieren
+#include <Wire.h>                          //Bibliothek für Bus System initialisieren
 
-#include "MeMCore.h"
-#include <Wire.h>
+MeLEDMatrix ledMx(PORT_1);                 //LED-Matrix (Display) Modul an Port 1 angeschlossen
+MeUltrasonicSensor ultraSensor(PORT_3);    //Ultraschallsensor an Port 3 angeschlossen
 
-MeLEDMatrix ledMx(PORT_1);
-MeUltrasonicSensor ultraSensor(PORT_3);
+float winkelge;                            //Variable für Winkelgeschwindigkeit als Ganszahlig definiert
+int abstand;                               //Variable für Abstand des Ultraschallsensors als Ganzzahlig definiert
 
-int winkelge;
-int abstand;
+float korre = 0.92;                        //Variable für rechten Korrekturfaktor als Dezimalzahl definiert
+float korli = 1.0;                         //Variable für linken Korrektrurfaktor als dezimalzahl definiert
+int speed = 120;                           //Variable für Geschwindigkeit als Integerzahl definiert und mit Startwert von 255 angegeben 
 
-float korre = 1.0;
-float korli = 1.0;
-int speed = 120;
+uint8_t links;                             //Ganzzahlige Variable mit 8 Bits für Geschwindigkeit des linken Motor
+uint8_t rechts;                            //Ganzzahlige Variable mit 8 Bits für Geschwindigkeit des rechten Motor
 
-uint8_t links;
-uint8_t rechts;
+MeDCMotor motorL(M1);                      //Motor links an Port M1 angeschlossen
+MeDCMotor motorR(M2);                      //Motor rechts an Port M2 angeschlossen
 
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
+MeGyro gyro;                               //Gyrosensor initialisiert mit Namen gyro
 
-MeGyro gyro;
-
-void setup()
+void setup()                               //Setup Schleife
 {
-  pinMode(7,INPUT);
-  ledMx.setBrightness(6);
-  ledMx.setColorIndex(1);
-  Serial.begin(115200);
-  gyro.begin();
+  pinMode(7,INPUT);                        //interner Knopf an Port 7 angeschlossen
+  ledMx.setBrightness(10);                 //Matrix LED Helligkeit von 10
+  ledMx.setColorIndex(1);                  //Matrix LED zeigt Hintergund weiß und Zahlen in blau
+  Serial.begin(115200);                    //Bus System startet
+  gyro.begin();                            //Bus System sartet Gyro Sensor
 }
 
-void loop()
+void loop()                                //Arbeitsschleife startet
 { 
-    gyro.update();
-    Serial.read();
-    winkelge = gyro.getGyroY();
-    ledMx.showNum(winkelge);
-    if(analogRead(7) < 100)
+    gyro.update();                         //Gyro Sensor ansteuern
+    Serial.read();                         //Gyro Sensor über Bus abfragen
+    winkelge = gyro.getGyroY();            //Winkelgeschwindigkeit um Y-Achse messen und als Variable speichern
+    ledMx.showNum(korre);                  //Winkelgeschwindigkeit auf LED-Matrix Modul anzeigen
+    if(analogRead(7) < 100)                //Starte, wenn Knopf gedrückt wird 
     {
-      delay(500);
-      gyro.update();
-      Serial.read();
-      winkelge = gyro.getGyroY(); 
-      ledMx.showNum(korli);
-      abstand = ultraSensor.distanceCm();
-      delay(500);
-    while(abstand>20)
+      delay(100);                          //Warte 100ms
+      gyro.update();                       //Gyrosensor ansteuern
+      Serial.read();                       //Gyrosensor lesen
+      winkelge = gyro.getGyroY();          //Winkelgeschwindigkeit um Y-Achse gemessen und als Variable gespeichert                
+      abstand = ultraSensor.distanceCm();  //Ultraschallsensor auslesen und Abstand in cm als Variable speichern
+      motorR.run(15);                      //Rechten Motor ansteuern (dreht noch nicht!)
+      motorL.run(-15);                     //Linken Motor ansteuern (dreht noch nicht!)
+      ledMx.showNum(korre);                //Korrekturfaktor für Motor rechts auf LED Matrix Modul anzeigen 
+      delay(2000);                         //Warte 2s
+    while(abstand>20)                      //Führe Schleife aus, wenn Ultraschallsensor Abstand größer 20cm misst
     {
-      links=speed*korli;
-      rechts=speed*korre;
-      motor2.run(rechts);
-      motor1.run(-links);
-      delay(250);
-      gyro.update();
-      Serial.read();
-      winkelge = gyro.getGyroY();
-      delay(250);
-      ledMx.showNum(korre);
-      if(winkelge>0){
-        korre=korre+0.01;
-      }
-      if(winkelge<0){
-        korre=korre-0.01;
-      }
-      abstand = ultraSensor.distanceCm();
-    }
-  motor1.stop();
-  motor2.stop();
-  delay(20);
-}
-}
+      links=speed*korli;                   //Geschwindigkeit linker Motor mit Korrekturfaktor angepasst
+      rechts=speed*korre;                  //Geschwindigkeit rechter Motor mit Korrekturfaktor angepasst
+      motorR.run(rechts);                  //Motor rechts dreht mit korregierter Geschwindigkeit
+      motorL.run(-links);                  //Motor links dreht mit korregierter Geschwindigkeit
+      delay(100);                          //Warte 100ms
+      gyro.update();                       //Gyro Sensor ansteuern
+      Serial.read();                       //Gyro Sensor über Bus abfragen
+      winkelge = gyro.getGyroY();          //Winkelgeschwindigkeit um Y-Achse messen und als Variable speichern
+      delay(100);                          //Warte 100ms
+      ledMx.showNum(korre);                //Korrekturfaktor rechts auf LED-Matrix Modul anzeigen
+      if (-5<winkelge<5){                  //Bedingung: Winkelgeschwindigkeit zwischen -5 und 5 (alle anderen Werte werden nicht beachtet, da fehlerbehaftet)
+        korre = korre+0.0025*winkelge;     //Korrekturfaktor rechts wird mit Faktor über Winkelgeschwindigkeit angepasst, so dass Winkelgeschwindigkeit sinkt
+      }                                    //Ende von Ausführung, wenn Bedingung in Zeile 60 erfüllt ist
+      abstand = ultraSensor.distanceCm();  //Ultraschallsensor auslesen und Abstand in cm als Variable speichern
+    }                                      //Starte ab Zeile 49 erneut, wenn Bedingung aus Zeile 48 erfüllt ist
+  motorL.stop();                           //Motor links wird gestoppt    
+  motorR.stop();                           //Motor rechts wird gestoppt
+  delay(20);                               //Warte 20ms
+}                                          //Ende von Ausführung, wenn Bedingung in Zeile 37 erfüllt ist
+}                                          //Starte ab Zeile 32 erneut 
 
